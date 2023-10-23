e9771d998340f44642e59c7e7d1781f8c18f57ba
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-23 02:02:16 +0200

    Update Task3.java
    updated with new class

--- src/progetto/task/Task3.java
+++ src/progetto/task/Task3.java
@@ -3,13 +3,15 @@ package progetto.task;
 import java.util.*;
 
 import progetto.model.Game;
+import progetto.model.GameTeamAssociations;
 import progetto.model.Team;
 
 public class Task3 {
 
     //creating a run method with the lists as arguments in order to satisfy the task's requests
-    public static void run(ArrayList<Game> game_List, ArrayList<Team> team_List, ArrayList<Game> newGame_List) {
-
+    public static void run(ArrayList<Game> game_List, ArrayList<Team> team_List, 
+    		 ArrayList<Game> newGame_List, GameTeamAssociations teamsToGames) {
+ 
 
     }
 
