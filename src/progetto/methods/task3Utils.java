2a9a3db6606fa7d723ea31392aabdf4007bd62cf
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-26 12:37:56 +0200

    Update task3Utils.java

--- src/progetto/methods/task3Utils.java
+++ src/progetto/methods/task3Utils.java
@@ -1,4 +1,10 @@
 package progetto.methods;
 
+import java.util.*;
+
+
+import progetto.model.Game;
+import progetto.model.Team;
+import progetto.model.GameTeamAssociations;
 public class task3Utils {
 }
