cdf8ebfc3de6cbd29b0f0369f706a048583730b2
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-26 12:37:45 +0200

    Update task2Utils.java
    update

--- src/progetto/methods/task2Utils.java
+++ src/progetto/methods/task2Utils.java
@@ -1,4 +1,22 @@
 package progetto.methods;
 
+import java.util.*;
+
+
+import progetto.model.Game;
+import progetto.model.Team;
+import progetto.model.GameTeamAssociations;
+
+//This Class contains all the methods that are needed in the Task2 Class
+//In this way the program is well written , more modular and easy to maintain
 public class task2Utils {
+
+    //Creating a method to find if there are at least p teams working in q games
+    public static boolean findTeamsInGames(GameTeamAssociations teamsToGames, int p, int q){
+
+
+    }
+
+
+
 }
