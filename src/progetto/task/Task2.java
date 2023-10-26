c4bd40d7fcfe53b4f4561af9f228dbb71438cc15
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-26 12:39:23 +0200

    Update Task2.java
    update

--- src/progetto/task/Task2.java
+++ src/progetto/task/Task2.java
@@ -2,9 +2,13 @@ package progetto.task;
 
 import java.util.*;
 
+//Importing the task2Utils class in order to use the methods for the task
+import progetto.methods.task2Utils;
+
+//Importing the objects classes directly into the task
 import progetto.model.Game;
-import progetto.model.GameTeamAssociations;
 import progetto.model.Team;
+import progetto.model.GameTeamAssociations;
 
 public class Task2 {
 
@@ -12,7 +16,22 @@ public class Task2 {
     public static void run(ArrayList<Game> game_List, ArrayList<Team> team_List, int p, int q,
     		GameTeamAssociations teamsToGames) {
 
+        //Initializing the 4 points with the information of the track, every point of task 2 is a boolean
+        boolean taskPoint1 = task2Utils.findTeamsInGames(teamsToGames, p, q);
+        boolean taskPoint2 = true;
+        boolean taskPoint3 = true;
+        boolean taskPoint4 = true;
 
+        //This is our output , if all of these conditions are valid we are printing YES
+        if (taskPoint1 && taskPoint2 && taskPoint3 && taskPoint4) System.out.println("YES");
+        //If only one of them is not valid we are printing NO
+        else System.out.println("NO");
+
+        //DEBUGGING
+        if(taskPoint1) System.out.println("The first condition is True");
+        if(taskPoint2) System.out.println("The second condition is True");
+        if(taskPoint3) System.out.println("The third condition is True");
+        if(taskPoint4) System.out.println("The fourth condition is True");
     }
 
 }
