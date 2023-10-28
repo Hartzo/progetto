aa9bd9a11c6a23b9987dca40a4d1dddeaa03148b
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-29 00:54:17 +0200

    Update progetto.java

--- src/progetto/progetto.java
+++ src/progetto/progetto.java
@@ -6,8 +6,8 @@ import progetto.model.*;
 import progetto.task.*;
 
 public class progetto {
-    //The main method is used to create what we need and store data from input
-    //After we store the data we call each of the tasks of the exam project
+    /* The main method is used to create what we need and store data from input
+     * After we store the data we call each of the tasks of the exam project */
     public static void main(String[] args) {
 
         //Creating an Array of Objects for each class of the project is a must for storing data
@@ -19,8 +19,8 @@ public class progetto {
         //Opening a Scanner with a try command is more professional, remembering to close the scanner is a good practice
         try (Scanner input = new Scanner(System.in)) {
 
-            //I chose a 'do-while' so we can call each task without closing the program
-            //if we enter an empty string the program closes
+            /* I chose a 'do-while' so we can call each task without closing the program
+             * if we enter an empty string the program closes */           
             do {
             	//Clearing the ArrayList at the start of each interaction
                 game_List.clear();
@@ -54,8 +54,8 @@ public class progetto {
 
                 //Reading last line to identify which task the program has to run
                 String[] tasks = input.nextLine().split(" ");
-                //Calling each run method of the tasks ; task 2 and 3 have further data to collect
-                //Using 'equalsIgnoreCase()' method as muscle memory ,we don't really need it in this program
+		        /* Calling each run method of the tasks ; task 2 and 3 have further data to collect
+		         * Using 'equalsIgnoreCase()' method even though ,we don't really need it in this program */
                 if (tasks[0].equalsIgnoreCase("TASK1"))
                     Task1.run(game_List, team_List, teamsToGames);
 
@@ -69,8 +69,8 @@ public class progetto {
                     int newGames = Integer.parseInt(tasks[1]);
                     //Creating a new array of objects for the new games
                     ArrayList<Game> newGame_List = new ArrayList<>();
-                    //Same 'for-loop' as before , we need to pass new games
-                    //Having a 'Reading' method makes the code easy to read
+                    /* Same 'for-loop' as before , we need to pass new games
+                     * Having a 'Reading' method makes the code easy to read */                     
                     for(int i = 0; i < newGames; i++) {
                         line = input.nextLine();
                         newGame_List.add(Game.ReadingData(line));
@@ -82,8 +82,8 @@ public class progetto {
 
             } while (input.hasNextLine());
 
-        //In the Exam track the input is considered always correct, so we don't need to look up for exceptions
-        //By the way a nice 'try-catch' for the scanner is a fancy way to close the program while printing an error
+        /* In the Exam track the input is considered always correct, so we don't need to look up for exceptions
+         * By the way a nice 'try-catch' for the scanner is a fancy way to close the program while printing an error */
         } catch (Exception e) {
             System.err.println("Error!" + e.getMessage());
             System.err.println("Closing program.");
