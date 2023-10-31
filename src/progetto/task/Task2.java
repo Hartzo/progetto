fb035d1be5e220e0892b9a9111dcaf6712f6bf86
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-31 01:40:53 +0100

    Update Task2.java

--- src/progetto/task/Task2.java
+++ src/progetto/task/Task2.java
@@ -10,15 +10,15 @@ import progetto.model.Game;
 import progetto.model.Team;
 import progetto.model.GameTeamAssociations;
 
-/* This is the Class for the Second Task , there are four conditions and each one of them 
- * calls a specific method to check the condition if it's true or false */ 
+/* This is the Class for the Second Task , there are four conditions and each one of them
+ * calls a specific method to check the condition if it's true or false */
 public class Task2 {
 
     //Creating a run method with the lists as arguments in order to satisfy the task's requests
     public static void run(ArrayList<Game> game_List, ArrayList<Team> team_List, int p, int q,
-    		GameTeamAssociations teamsToGames) {
+                           GameTeamAssociations teamsToGames) {
 
-        //Initializing the 4 points as booleans with the track's informations 
+        //Initializing the 4 points as booleans with the track's information
         boolean taskPoint1 = task2Utils.findTeamsInGames(teamsToGames, p, q);
         boolean taskPoint2 = task2Utils.findGamesWithSharedTeams(teamsToGames, q, game_List);
         boolean taskPoint3 = task2Utils.findUnderfundedGames(game_List, teamsToGames, p, q);
@@ -26,7 +26,7 @@ public class Task2 {
 
         //This is our output , if all of these conditions are valid we are printing YES
         if (taskPoint1 && taskPoint2 && taskPoint3 && taskPoint4) System.out.println("YES");
-        //If only one of them is not valid we are printing NO
+            //If only one of them is not valid we are printing NO
         else System.out.println("NO");
 
         /*DEBUGGING
