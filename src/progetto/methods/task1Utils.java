60e6b710550efe7e4019d4cbde9d396b6cadab38
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-31 19:45:10 +0100

    Update task1Utils.java

--- src/progetto/methods/task1Utils.java
+++ src/progetto/methods/task1Utils.java
@@ -64,6 +64,7 @@ public class task1Utils {
                 .count();
     }
 
+    //Method created to find the exact team with the max number of Devs, it returns the string team_Code
     public static String findTeamWithMaxDevs(ArrayList<Team> team_List) {
         //Creating a HashMap to store the count of devs for each team
         Map<String, Integer> teamDevsCount = new HashMap<>();
@@ -77,28 +78,10 @@ public class task1Utils {
             teamDevsCount.put(teamCode, teamDevsCount.getOrDefault(teamCode, t.getNum_devs()));
         }
         //Finding the maximum count of Devs among all Teams
-        int maxCount = Collections.max(teamDevsCount.values());
-
-        //Creating a list to store team codes with the maximum count of devs
-        List<String> teamWithMaxDevs = new ArrayList<>();
-
-        //Iterating through the entries in the HashMap
-        for (Map.Entry<String, Integer> entry : teamDevsCount.entrySet()) {
-            //If the count of devs for a team matches the maximum count, add it to the list
-            if (entry.getValue() == maxCount) {
-                teamWithMaxDevs.add(entry.getKey());
-            }
-        }
-        //Sorting the list of teams with the maximum count, using collections makes it simpler
-        Collections.sort(teamWithMaxDevs);
-        //Check if the list is not empty
-        if (!teamWithMaxDevs.isEmpty()) {
-            return teamWithMaxDevs.get(0); //Returning the first team code in the sorted list
-        } else {
-            return "";//If the list is empty, return an empty string as requested from the task
-        }
+        return getCounter(teamDevsCount);
     }
 
+    //Method to find the first team that has worked for the entirety of its developing time
     public static String findTeamWithAllDT(GameTeamAssociations teamsToGames,
                                            ArrayList<Game> game_List) {
         //Creating a HashMap to store the count of teams with all the Developing time
@@ -123,29 +106,11 @@ public class task1Utils {
                 }
             }
         }
-
         //Finding the maximum count of Devs among all Teams
-        int maxCount = Collections.max(teamDTCount.values());
-        //Creating a list to store team codes with the all developing time
-        List<String> teamWithAllDT = new ArrayList<>();
-
-        for (Map.Entry<String, Integer> entry : teamDTCount.entrySet()) {
-            if (entry.getValue() == maxCount) {
-                teamWithAllDT.add(entry.getKey());
-            }
-        }
-
-        //Sorting the list of teams with the whole Developing Time , using collections makes it simpler
-        Collections.sort(teamWithAllDT);
-
-        //Check if the list is not empty
-        if (!teamWithAllDT.isEmpty()) {
-            return teamWithAllDT.get(0); //Returning the first team code in the sorted list
-        } else {
-            return "";//If the list is empty, return an empty string as requested from the task
-        }
+        return getCounter(teamDTCount);
     }
 
+    //This method returns the common skill among the teams in our team_List
     public static String findMostFrequentSkill(ArrayList<Team> team_List) {
         //Create a map to store skills and their counts
         Map<String, Integer> skillCount = new HashMap<>();
@@ -219,6 +184,33 @@ public class task1Utils {
         }
         //The Counter is our result
         return counter;
+    }
+
+    //This method has been created to prevent duplications , two methods are using it
+    private static String getCounter(Map<String, Integer> mapCount){
+        //Max Counter for our sorting
+        int maxCount = Collections.max(mapCount.values());
+
+        //Creating a list to store team codes
+        List<String> dataString = new ArrayList<>();
+
+        //Adding the teams with the values to the map
+        for (Map.Entry<String, Integer> entry : mapCount.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                dataString.add(entry.getKey());
+            }
+        }
+
+        //Sorting the list of teams , using collections makes it simpler
+        Collections.sort(dataString);
+
+        //Check if the list is not empty
+        if (!dataString.isEmpty()) {
+            return dataString.get(0); //Returning the first team code in the sorted list
+        } else {
+            return "";//If the list is empty, return an empty string as requested from the task
+        }
+
     }
 
 }
