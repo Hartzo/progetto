e3d7ca33ebc19ba08d6a3acd1ab5a9db0473f2c7
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-31 01:41:09 +0100

    Update Task3.java

--- src/progetto/task/Task3.java
+++ src/progetto/task/Task3.java
@@ -15,22 +15,22 @@ import progetto.model.Team;
 public class Task3 {
 
     //Creating a run method with the lists as arguments in order to satisfy the task's requests
-    public static void run(ArrayList<Game> game_List, ArrayList<Team> team_List, 
-    		 ArrayList<Game> newGame_List, GameTeamAssociations teamsToGames) {
-    	
-    	//Initializing boolean variables with the methods
+    public static void run(ArrayList<Game> game_List, ArrayList<Team> team_List,
+                           ArrayList<Game> newGame_List, GameTeamAssociations teamsToGames) {
+
+        //Initializing boolean variables with the methods
         boolean taskPoint1 = task3Utils.findUnassignedTeams(team_List, newGame_List, teamsToGames);
         boolean taskPoint2 = task3Utils.findNewGamesTimeOverlap(game_List, newGame_List);
-        boolean taskPoint3 = task3Utils.findContainsUnderfundedTitles(newGame_List, teamsToGames);
-    	
-    	//If all points are true then we print "VALID" as requested
+        boolean taskPoint3 = task3Utils.findContainsUnderfundedGames(newGame_List, teamsToGames);
+
+        //If all points are true then we print "VALID" as requested
         if(taskPoint1 && taskPoint2 && taskPoint3) System.out.println("VALID");
         //If just one condition is false then we print "NOT VALID"
         else System.out.println("NOT VALID");
-        
+
         /*DEBUGGING
         if(taskPoint1) System.out.println("The First condition is True");
-        if(taskPoint2) System.out.println("The Second condition is True"); 
+        if(taskPoint2) System.out.println("The Second condition is True");
         if(taskPoint3) System.out.println("The Third condition is True");*/
     }
 
