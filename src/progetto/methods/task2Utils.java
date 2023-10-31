427a13b8a00b3020dfcdf395fd2be1be2885694c
Giuseppe Priore <137206026+Hartzo@users.noreply.github.com>
2023-10-31 01:41:52 +0100

    Update task2Utils.java

--- src/progetto/methods/task2Utils.java
+++ src/progetto/methods/task2Utils.java
@@ -27,90 +27,84 @@ public class task2Utils {
 
 
     public static boolean findGamesWithSharedTeams(GameTeamAssociations teamsToGames, int q, ArrayList<Game> game_List) {
-    	
-    	HashMap<String, List<String>> teamsInGames = new HashMap<>();
-    	
-    
-    	for(String gameCode : teamsToGames.getAssociations().keySet()) {
-    		List<String> teamsCode = teamsToGames.getTeamsForGame(gameCode);
-    		
-    		teamsInGames.put(gameCode, teamsCode);
-    	}
-    	
-    	HashMap<String, Integer> gamesYearsDev = new HashMap<>();
-    	
-    	for(String gameCode : teamsInGames.keySet()) {
-    		 
-    		int finishYear = Game.fromCode(game_List, gameCode).getGame_finish_year();
-    		int startYear = Game.fromCode(game_List, gameCode).getGame_start_year();
-    		gamesYearsDev.put(gameCode, (finishYear - startYear));
-    	}
-    	
-    	
-    	//System.out.println(teamsInGames);
-    	
-    	//System.out.println(gamesYearsDev);
-    	
-    	//Counter for the number of games with the same team and the same dev time 
-    	int sharedTeamsAndDevelopmentCounter = 0;
 
-    	//Iterating through the games
-    	for (String gameCode : teamsInGames.keySet()) {
-    	    List<String> teamsCode = teamsInGames.get(gameCode);
-    	    int gameDevelopmentYears = gamesYearsDev.get(gameCode);
+        HashMap<String, List<String>> teamsInGames = new HashMap<>();
 
-    	    //Confronting the games
-    	    for (String otherGameCode : teamsInGames.keySet()) {
-    	        if (!gameCode.equals(otherGameCode)) {
-    	            List<String> otherTeamsCode = teamsInGames.get(otherGameCode);
-    	            int otherGameDevelopmentYears = gamesYearsDev.get(otherGameCode);
-
-    	            //Check if at least one team is in common and dev time is the same 
-    	            boolean hasCommonTeam = false;
-
-    	            for (String teamCode : teamsCode) {
-    	                if (otherTeamsCode.contains(teamCode)) {
-    	                    hasCommonTeam = true;
-    	                    break;
-    	                }
-    	            }
-
-    	            if (hasCommonTeam && gameDevelopmentYears == otherGameDevelopmentYears) {
-    	                sharedTeamsAndDevelopmentCounter++;
-    	                break; //If conditions are set we exit the loop 
-    	            }
-    	        }
-    	    }
-    	}
-
-    	//This is our result with the last check with q games 
-    	return sharedTeamsAndDevelopmentCounter >= q;
-
-    }
-
-    
-    public static boolean findUnderfundedGames(ArrayList<Game> game_List,
-                                    GameTeamAssociations teamsToGames, int p , int q) {
-    	
-    	int numUnderfundedGames = task1Utils.isUnderfunded(game_List, teamsToGames);
-        /*DEBUGGING
-    	System.out.println("gli underfunded sono " + numUnderfundedGames);*/
-    	return numUnderfundedGames >= p && numUnderfundedGames <= q;
-   	
-    }
-    
-    public static boolean findGamesWithSharedTSkill(GameTeamAssociations teamsToGames, int p, ArrayList<Team> team_List) {
-    	// Contatore per tenere traccia del numero di titoli con team con skill in comune
-        int titlesWithSharedSkills = 0;
-        
-        // Itera attraverso i titoli
-        for (String gameCode : teamsToGames.getAssociations().keySet()) {
-            // Ottieni i team associati a questo titolo
+        for(String gameCode : teamsToGames.getAssociations().keySet()) {
             List<String> teamsCode = teamsToGames.getTeamsForGame(gameCode);
-            
-            // Controlla se ci sono almeno due team associati
+
+            teamsInGames.put(gameCode, teamsCode);
+        }
+
+        HashMap<String, Integer> gamesYearsDev = new HashMap<>();
+
+        for(String gameCode : teamsInGames.keySet()) {
+
+            int finishYear = Game.fromCode(game_List, gameCode).getGame_finish_year();
+            int startYear = Game.fromCode(game_List, gameCode).getGame_start_year();
+            gamesYearsDev.put(gameCode, (finishYear - startYear));
+        }
+
+        //Counter for the number of games with the same team and the same dev time
+        int sharedTeamsAndDevelopmentCounter = 0;
+
+        //Iterating through the games
+        for (String gameCode : teamsInGames.keySet()) {
+            List<String> teamsCode = teamsInGames.get(gameCode);
+            int gameDevelopmentYears = gamesYearsDev.get(gameCode);
+
+            //Confronting the games
+            for (String otherGameCode : teamsInGames.keySet()) {
+                if (!gameCode.equals(otherGameCode)) {
+                    List<String> otherTeamsCode = teamsInGames.get(otherGameCode);
+                    int otherGameDevelopmentYears = gamesYearsDev.get(otherGameCode);
+
+                    //Check if at least one team is in common and dev time is the same
+                    boolean hasCommonTeam = false;
+
+                    for (String teamCode : teamsCode) {
+                        if (otherTeamsCode.contains(teamCode)) {
+                            hasCommonTeam = true;
+                            break;
+                        }
+                    }
+
+                    if (hasCommonTeam && gameDevelopmentYears == otherGameDevelopmentYears) {
+                        sharedTeamsAndDevelopmentCounter++;
+                        break; //If conditions are set we exit the loop
+                    }
+                }
+            }
+        }
+
+        //This is our result with the last check with q games
+        return sharedTeamsAndDevelopmentCounter >= q;
+
+    }
+
+    //Creating a method to check if there is a number of underfunded games between p and q
+    public static boolean findUnderfundedGames(ArrayList<Game> game_List,
+                                               GameTeamAssociations teamsToGames, int p , int q) {
+
+        int numUnderfundedGames = task1Utils.isUnderfunded(game_List, teamsToGames);
+
+        return numUnderfundedGames >= p && numUnderfundedGames <= q;
+
+    }
+
+    //Creating a method to check if the teams associated to a game have common skills
+    public static boolean findGamesWithSharedTSkill(GameTeamAssociations teamsToGames, int p, ArrayList<Team> team_List) {
+        //Counter to keep track of the number of games with teams with common skills
+        int titlesWithSharedSkills = 0;
+
+        //Iterate through games
+        for (String gameCode : teamsToGames.getAssociations().keySet()) {
+            //Getting the associated teams
+            List<String> teamsCode = teamsToGames.getTeamsForGame(gameCode);
+
+            //Check if there are at least two teams associated to a game
             if (teamsCode.size() >= 2) {
-                // Verifica se almeno due team hanno skill in comune
+                //Check if at least two games share a common skill
                 boolean sharedSkillsFound = false;
                 for (int i = 0; i < teamsCode.size() - 1; i++) {
                     for (int j = i + 1; j < teamsCode.size(); j++) {
@@ -118,8 +112,8 @@ public class task2Utils {
                         String teamCode2 = teamsCode.get(j);
                         Team team1 = Team.fromCode(team_List, teamCode1);
                         Team team2 = Team.fromCode(team_List, teamCode2);
-                        
-                        // Verifica se i team hanno skill in comune
+
+                        //Calling the method to verify the strings of skills
                         if (team1.haveSharedSkills(team2)) {
                             sharedSkillsFound = true;
                             break;
@@ -129,17 +123,15 @@ public class task2Utils {
                         break;
                     }
                 }
-                
+
                 if (sharedSkillsFound) {
                     titlesWithSharedSkills++;
                 }
             }
         }
-        
-        // Verifica se il numero di titoli con team con skill in comune è almeno p
+
+        //The task-point requires if the teams that are sharing the same skill are at least p
         return titlesWithSharedSkills >= p;
     }
-
-
 
 }
